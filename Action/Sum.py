3e59068f6800607a10b7115478c13a1d1c4ca77b
alinashev <al.t.shevchenko@gmail.com>
2021-12-08 12:49:14 +0200

    Added requirement

--- Action/Sum.py
+++ Action/Sum.py
@@ -1,12 +1,19 @@
+import numpy as np
 from Action.Action import Action
 
 
 class Sum(Action):
     def execute(self) -> None:
         self.get_sum()
+        self.get_array()
 
     def get_sum(self):
         sum = 0
         for i in range(0, 10):
             sum = sum + i
         print('sum ', sum)
+
+    def get_array(self):
+        a = np.arange(0, 10, 2)
+        for i in a:
+            print(i)
